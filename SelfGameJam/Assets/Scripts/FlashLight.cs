f5e5ff71a9524de65a453cb0812d09748252c8ad
JamsterMan <ben.brad.h@gmail.com>
2022-01-06 20:00:24 -0500

    added art
    -added art
    -messed with shadows

--- SelfGameJam/Assets/Scripts/FlashLight.cs
+++ SelfGameJam/Assets/Scripts/FlashLight.cs
@@ -6,28 +6,18 @@ using UnityEngine.Tilemaps;
 public class FlashLight : MonoBehaviour
 {
     public LayerMask playerLayer;
-    private Vector3 lastMousePos;
     private float angle;
 
-    private void Start()
-    {
-        lastMousePos = Vector3.zero;
-    }
 
     void Update()
     {
-
         Vector3 mousePos = Input.mousePosition;
-        if (mousePos != lastMousePos) {
-            lastMousePos = mousePos;
-            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-            mousePos.x -= objectPos.x;
-            mousePos.y -= objectPos.y;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x -= objectPos.x;
+        mousePos.y -= objectPos.y;
 
-            angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        }
-
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     public float GetFlashLightAngle()
