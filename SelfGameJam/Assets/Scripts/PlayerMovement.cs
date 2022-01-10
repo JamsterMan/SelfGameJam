dabe1b3fae844774cf0e5669a4f5abbbf1588001
JamsterMan <ben.brad.h@gmail.com>
2022-01-09 21:23:54 -0500

    added esc quit

--- SelfGameJam/Assets/Scripts/PlayerMovement.cs
+++ SelfGameJam/Assets/Scripts/PlayerMovement.cs
@@ -41,6 +41,12 @@ public class PlayerMovement : MonoBehaviour
             jump = true;
         }
 
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Debug.Log("Quit Game");
+            Application.Quit();
+        }
+
     }
 
     private void FixedUpdate()
